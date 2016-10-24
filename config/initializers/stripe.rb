0676d3b5bf38f153b7de7d28657c1e0a98819888
Nithin Manari Venkata Satya <nithin.mannari@gmail.com>
2016-10-24 20:37:42 +0000

    Testing emails

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,11 @@
-Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
-    :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
-}
-
-# Rails.configuration.stripe = { 
-#   :publishable_key => 'pk_test_DgLEOWfMK5G83DJ4DMRILmjX', 
-#   :secret_key => 'sk_test_8E1famiG3dnF06qTrzJO8uq5'
+# Rails.configuration.stripe = {
+#     :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+#     :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 # }
+
+Rails.configuration.stripe = { 
+  :publishable_key => 'pk_test_DgLEOWfMK5G83DJ4DMRILmjX', 
+  :secret_key => 'sk_test_8E1famiG3dnF06qTrzJO8uq5'
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
