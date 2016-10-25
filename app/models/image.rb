40e61589307b94b5d7ebd54f680243b99e905fd7
Nithin Manari Venkata Satya <nithin.mannari@gmail.com>
2016-10-25 02:26:55 +0000

    Error Handling

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,7 +6,7 @@ class Image < ActiveRecord::Base
   private
   
   def picture_size
-    if picture_size > 5.megabytes
+    if picture.size > 5.megabytes
       errors.add(:picture, "should be less than 5 MB")
     end  
   end
